798fd4637fbb30604187cd57a18fa3f84ecfe5cd
SARVESS <sarveshsri76@gmail.com>
2020-08-10 02:08:06 +0530

    Updates

---
+++ precache-manifest.8f2a8238d9f28457a4d9a867265e2cc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f394071cad8dce3560a75b1f5ec8d8cc",
+    "url": "/Tooltip-React/index.html"
+  },
+  {
+    "revision": "a8667ee801549f09a982",
+    "url": "/Tooltip-React/static/css/main.f5dc34fc.chunk.css"
+  },
+  {
+    "revision": "abbb1af2e914eedbde2a",
+    "url": "/Tooltip-React/static/js/2.ada14da4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tooltip-React/static/js/2.ada14da4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8667ee801549f09a982",
+    "url": "/Tooltip-React/static/js/main.b5dc149d.chunk.js"
+  },
+  {
+    "revision": "f307035998dc47dfb7ac",
+    "url": "/Tooltip-React/static/js/runtime-main.19dc6ffd.js"
+  }
+]);
